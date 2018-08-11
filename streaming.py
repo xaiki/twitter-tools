9073397d7257b44ad6b91dcc3180d3d7a05c5c4f
Niv Sardi <xaiki@evilgiggle.com>
2018-08-10 21:02:56 -0300

    use config.py in streaming.py and use getopt to pass args

--- streaming.py
+++ streaming.py
@@ -1,14 +1,48 @@
+import sys
+import re
+from getopt import getopt, GetoptError
+
 from tweepy.streaming import StreamListener
 from tweepy import OAuthHandler
 from tweepy import Stream
 from save_to_db import save_to_db
+from config import get_config
 
-# Authentication details.
-consumer_key = ""
-consumer_secret = ""
-access_token = ""
-access_token_secret = ""
+r = re.compile("\s+")
+db = "mysql"
+ids = []
+track = []
+argv = sys.argv
+config_file = 0
 
+if ((sys.argv[0]).find(".py") != -1):
+    argv = sys.argv[1:]
+
+def usage():
+    print "usage: [-c|--config config.json] [-f|--file id.csv] [-i|--ids id1,id2,id3...] [-t|--track hash1,hash2,hash3...] [-d|--db]"
+
+try:
+    opts, args = getopt(argv, "c:f:i:t:D:", ["config", "file", "ids", "track"])
+except GetoptError:
+    usage()
+    sys.exit(2)
+for opt, arg in opts:
+    if opt in ("-c", "--config"):
+        config_file = arg
+    if opt in ("-D", "--db"):
+        db = arg
+    if opt in ("-i", "--ids"):
+        ids += r.sub(",", arg).split(',')
+    if opt in ("-t", "--track"):
+        track += r.sub(",", arg).split(',')
+
+    if opt in ("-f", "--file"):
+        with open(arg) as f:
+            for row in f:
+                ids.append(row)
+
+config = get_config(config_file)
+ids += args
 
 # This is the listener, resposible for receiving data
 class StdOutListener(StreamListener):
@@ -24,13 +58,8 @@ class StdOutListener(StreamListener):
 
 if __name__ == "__main__":
     l = StdOutListener()
-    auth = OAuthHandler(consumer_key, consumer_secret)
-    auth.set_access_token(access_token, access_token_secret)
-
-    ids = []
-    with open("ids.csv") as f:
-        for row in f:
-            ids.append(row)
+    auth = OAuthHandler(config['consumer_key'], config['consumer_secret'])
+    auth.set_access_token(config['access_token'], config['access_token_secret'])
 
     stream = Stream(auth, l)
     stream.filter(ids)
