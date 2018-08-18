d4161e9d76f4ebe3e6b6d0d08202af6ad887225f
Niv Sardi <xaiki@evilgiggle.com>
2018-08-18 14:28:47 -0300

    move arg parsing to config.py and use it in streaming.py

--- config.py
+++ config.py
@@ -1,7 +1,117 @@
-from random import randint
+import re
+import sys
 import json
 
+from getopt import getopt, GetoptError
+from random import randint
+
+r = re.compile("\s+")
+filename = None
+
 def get_config(filename):
+    d = load_json(filename)
+    return d[randint(0, d.__len__() - 1)]
+
+def o2u(option):
+    args = "-%s|--%s" % (option['short'].replace(':', ''), option['long'])
+    usage = option['usage'] % args
+    return "\t[%s]\t%s\n" % (usage, option['doc'])
+
+def usage(name, options):
+    print "usage: %s" %name
+    return reduce(lambda acc, cur: acc + o2u(cur), options, "")
+
+def identity(a):
+    return a
+
+def load_json(filename):
     with open(filename) as data:
-        d = json.load(data)
-        return d[randint(0, d.__len__() - 1)]
+        return json.load(data)
+
+def load_row_file(file):
+    ret = []
+    with open(file) as f:
+        for row in f:
+            ret.append(row)
+    return ret
+
+def parse_db(arg):
+    try: 
+        db_driver, filename = arg.split(':')
+    except ValueError:
+        db_driver = arg
+        filename = None
+    finally:
+        if db_driver == "mysql":
+            from DB.mysql import Driver
+            filename = filename or "mysql://"
+        elif db_driver == "sqlite":
+            from DB.sqlite import Driver
+            filename = filename or "twitter.sqlite"
+        elif db_driver == "elasticsearch":
+            from DB.elasticsearch import Driver
+            filename = filename or "ec://"
+        elif db_driver == "pynx":
+            from DB.pynx import Driver
+            filename = filename or "graph.gexf"
+        else:
+            print "ERROR could not find db driver for ", db_driver
+            sys.exit(-2)
+        return Driver(filename)
+
+def parse_comas(arg):
+    return r.sub(",", arg).split(',')
+
+def make_short(o):
+    if o.has_key('parse'): return o['short'] + ':'
+    return o['short']
+
+def make_long(o):
+    if o.has_key('parse'): return o['long'] + '='
+    return o['long']
+
+def parse_args(options):
+    argv = sys.argv
+
+    if ((sys.argv[0]).find(".py") != -1):
+        argv = sys.argv[1:]
+
+    shorts = "".join(map(make_short, options))
+    longs = map(make_long, options)
+    sopthash = dict(map(lambda o: (o['short'], o), options))
+    lopthash = dict(map(lambda o: (o['long'], o), options))
+    parsed = {}
+
+    def usage():
+        print "usage: %s" % "\n".join(map(o2u, options))
+
+    def get_key(opt):
+        try:
+            return sopthash[opt[1:]]
+        except KeyError:
+            return lopthash[opt[2:]]
+
+    try:
+        opts, args = getopt(argv, shorts, longs)
+    except GetoptError:
+        usage()
+        sys.exit(2)
+    for opt, arg in opts:
+        try:
+            k = get_key(opt)
+            parsed.update({k['long']: k['parse'](arg)})
+        except KeyError:
+            print "couldn't parse arg: %s, %s" % (opt, arg)
+
+    print "parsed: %s" % (parsed)
+    return parsed
+
+CONFIG_FILE = {'long': 'config', 'short': 'c', 'usage': '%s config.json', 'doc': 'config file', 'parse': load_json}
+IDS = {'long': 'ids', 'short': 'i', 'usage': '%s "id1,id2,id3"', 'doc': 'twitter user ids', 'parse': parse_comas}
+USERS = {'long': 'user', 'short': 'u', 'usage': '%s "user1,user2,usr3"', 'doc': 'twitter usernames', 'parse': parse_comas}
+TERMS = {'long': 'track', 'short': 't', 'usage': '%s "term1,term2,term3"', 'doc': 'terms to track', 'parse': parse_comas}
+DBS =  {'long': 'database', 'short': 'D', 'usage': '%s [mysql|sqlite|elasticsearch]', 'doc': 'database system to use', 'parse': parse_db}
+options = [CONFIG_FILE, IDS, USERS, TERMS, DBS]
+
+if __name__ == '__main__':
+    parse_args(options)
--- streaming.py
+++ streaming.py
@@ -7,58 +7,11 @@ from urllib3.exceptions import ProtocolError
 from tweepy.streaming import StreamListener
 from tweepy import OAuthHandler
 from tweepy import Stream
-from config import get_config
+
+import config as c
 
 r = re.compile("\s+")
-db_driver = "mysql"
-ids = []
-track = []
-argv = sys.argv
-config_file = 0
-filename = None
 
-if ((sys.argv[0]).find(".py") != -1):
-    argv = sys.argv[1:]
-
-def usage():
-    print "usage: [-c|--config config.json] [-f|--file id.csv] [-i|--ids id1,id2,id3...] [-t|--track hash1,hash2,hash3...] [-d|--db]"
-
-try:
-    opts, args = getopt(argv, "c:f:i:t:D:", ["config", "file", "ids", "track"])
-except GetoptError:
-    usage()
-    sys.exit(2)
-for opt, arg in opts:
-    if opt in ("-c", "--config"):
-        config_file = arg
-    if opt in ("-D", "--db"):
-        db_driver = arg
-    if opt in ("-i", "--ids"):
-        ids += r.sub(",", arg).split(',')
-    if opt in ("-t", "--track"):
-        track += r.sub(",", arg).split(',')
-
-    if opt in ("-f", "--file"):
-        filename=arg
-
-    config = get_config(config_file)
-ids += args
-
-if db_driver == "mysql":
-    from DB.mysql import Driver
-    filename = filename or "mysql://"
-elif db_driver == "sqlite":
-    from DB.sqlite import Driver
-    filename = filename or "twitter.sqlite"
-elif db_driver == "elasticsearch":
-    from DB.elasticsearch import Driver
-    filename = filename or "ec://"
-elif db_driver == "pynx":
-    from DB.pynx import Driver
-    filename = filename or "graph.gexf"
-else:
-    print "ERROR could not find db driver for ", db_driver
-    sys.exit(-2)
 # This is the listener, resposible for receiving data
 class StdOutListener(StreamListener):
 
@@ -72,11 +25,16 @@ class StdOutListener(StreamListener):
         print status
 
 if __name__ == "__main__":
+    opts = c.parse_args([c.CONFIG_FILE, c.IDS, c.TERMS, c.DBS])
+    db = opts['database']
+    config = opts['config'][0]
+    ids = opts['ids']
+    track = opts['track']
+
     l = StdOutListener()
     auth = OAuthHandler(config['consumer_key'], config['consumer_secret'])
     auth.set_access_token(config['access_token'], config['access_token_secret'])
 
-    db = Driver(filename)
     def signal_handler(sig, frame):
         db.close()
         sys.exit(0)
