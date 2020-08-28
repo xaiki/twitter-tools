d2746c00ddbf71c73d7274134d4e77e615b030ac
Niv Sardi <xaiki@evilgiggle.com>
2020-08-28 03:44:58 -0300

    streaming: fix code and add error handeling
    Signed-off-by: Niv Sardi <xaiki@evilgiggle.com>

--- streaming.py
+++ streaming.py
@@ -5,10 +5,9 @@ stream tweets to database driver and stdout
 import signal
 import sys
 
+import tweepy
 from urllib3.exceptions import ProtocolError
 from tweepy.streaming import StreamListener
-from tweepy import OAuthHandler
-from tweepy import Stream
 
 import config as c
 
@@ -17,13 +16,14 @@ class StdOutListener(StreamListener):
     listener, resposible for receiving data
     """
     def __init__(self, database):
-        super(self)
+        super(StdOutListener, self).__init__()
         self.database = database
 
     def on_status(self, status):
         """
         a twitter status has been recieved
         """
+
         tweet_url = (
             "http://twitter.com/" + status.user.screen_name + "/status/" + status.id_str
         )
@@ -35,22 +35,36 @@ class StdOutListener(StreamListener):
         """
         error handler
         """
-        print(status)
+        print("error", status)
 
 def run():
     """
     main entry point
     """
-    opts = c.parse_args([c.CONFIG_FILE, c.IDS, c.TERMS, c.DBS])
+    opts = c.parse_args([c.CONFIG_FILE, c.IDS, c.DBS, c.TERMS])
 
     database = opts.db
     config = opts.config[0]
-    ids = opts.ids or []
-    track = opts.track or []
 
-    stdout = StdOutListener(database)
-    auth = OAuthHandler(config["consumer_key"], config["consumer_secret"])
+    stream_config = {
+        "follow": opts.ids or None,
+        "track": opts.track or None
+
+    }
+
+    listener = StdOutListener(database)
+
+    auth = tweepy.OAuthHandler(config["consumer_key"], config["consumer_secret"])
     auth.set_access_token(config["access_token"], config["access_token_secret"])
+
+
+    api = tweepy.API(auth)
+    # test authentication
+    try:
+        api.verify_credentials()
+        print("Authentication OK")
+    except:
+        print("Error during authentication")
 
     def signal_handler():
         database.close()
@@ -58,11 +72,11 @@ def run():
 
     signal.signal(signal.SIGINT, signal_handler)
 
-    stream = Stream(auth, stdout)
-    print("STREAM", ids, track)
+    stream = tweepy.Stream(auth = auth, listener = listener)
+    print("STREAM", stream_config)
     while True:
         try:
-            stream.filter(follow=ids, track=track)
+            stream.filter(**stream_config)
         except ProtocolError:
             pass
 
