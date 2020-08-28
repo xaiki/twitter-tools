cd6a1faba9d3c45473cbde4fd75385e49e4a45fb
Niv Sardi <xaiki@evilgiggle.com>
2020-08-28 03:41:42 -0300

    port DB to python3
    Signed-off-by: Niv Sardi <xaiki@evilgiggle.com>

--- DB/generic.py
+++ DB/generic.py
@@ -3,16 +3,16 @@ class DB:
         self.name = "Generic DB Driver"
 
     def getTweets(self):
-        print "NOT IMPLEMENTED"
+        print("NOT IMPLEMENTED")
 
     def writeSuccess(self, path):
-        print "NOT IMPLEMENTED"
+        print("NOT IMPLEMENTED")
 
     def markDeleted(self, path):
-        print "NOT IMPLEMENTED"
+        print("NOT IMPLEMENTED")
 
     def getLogs(self):
-        print "NOT IMPLEMENTED"
+        print("NOT IMPLEMENTED")
 
     def save(self, url, status):
-        print "NOT IMPLEMENTED"
+        print("NOT IMPLEMENTED")
