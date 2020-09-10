1c152a63e0fe1a3d37312dfb1307a6aea083c226
Niv Sardi <xaiki@evilgiggle.com>
2020-09-10 11:15:48 -0300

    DB/generic: now that all goes through multi, really generic is about to disappear
    Signed-off-by: Niv Sardi <xaiki@evilgiggle.com>

--- DB/generic.py
+++ DB/generic.py
@@ -3,18 +3,3 @@ import logging
 class DB:
     def __init__(self):
         self.name = "Generic DB Driver"
-
-    def getTweets(self):
-        logging.warning("NOT IMPLEMENTED")
-
-    def writeSuccess(self, path):
-        logging.warning("NOT IMPLEMENTED")
-
-    def markDeleted(self, path):
-        logging.warning("NOT IMPLEMENTED")
-
-    def getLogs(self):
-        logging.warning("NOT IMPLEMENTED")
-
-    def save(self, url, status):
-        logging.warning("NOT IMPLEMENTED")
